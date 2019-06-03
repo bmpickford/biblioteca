b717b9f68e05e97723420ec48bac2cd9fbb8c4b6
Benjamin Pickford <b.mpickford@outlook.com>
2019-06-03 17:17:47 +1000

    1.3 Add book Author and Year tests

--- test/com/twu/biblioteca/BookTest.java
+++ test/com/twu/biblioteca/BookTest.java
@@ -24,4 +24,34 @@ public class BookTest {
         Book book = new Book("\nthis-is_a\\book");
         assertThat(book.Name(), is("\nthis-is_a\\book"));
     }
+
+    @Test
+    public void TestBookAuthor() {
+        Book book = new Book("Jonothan Livingstone Seagull", "Richard Baci");
+        assertThat(book.Author(), is("Richard Baci"));
+    }
+
+    @Test
+    public void TestBookEmptyAuthor() {
+        Book book = new Book("Jonothan Livingstone Seagull", "");
+        assertThat(book.Author(), is(""));
+    }
+
+    @Test
+    public void TestBookYear() {
+        Book book = new Book("Jonothan Livingstone Seagull", "Richard Baci", 1970);
+        assertThat(book.YearPublished(), is(1970));
+    }
+
+    @Test
+    public void TestBookNegativeYear() {
+        Book book = new Book("Jonothan Livingstone Seagull", "Richard Baci", -1970);
+        assertThat(book.YearPublished(), is(-1970));
+    }
+
+    @Test
+    public void TestBookZeroYear() {
+        Book book = new Book("Jonothan Livingstone Seagull", "Richard Baci", 0);
+        assertThat(book.YearPublished(), is(0));
+    }
 }
